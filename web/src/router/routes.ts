cf2a96d1f226badd749837fcfad85aa47e680087
dkenzik <dkenzik@five9.com>
2025-11-28 08:05:41 -0500

    power-on animation

--- web/src/router/routes.ts
+++ web/src/router/routes.ts
@@ -3,9 +3,12 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    component: () => import('pages/Landing.vue'),
+  },
+  {
+    path: '/resume',
     component: () => import('pages/Index.vue'),
   },
 ];
 
 export default routes;
-
