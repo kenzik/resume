cff01e796a355b9a857a6d04e2a4f5feaeb207b3
dkenzik <dkenzik@five9.com>
2025-11-27 21:25:04 -0500

    updated resume env data in vite and contact formatter

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -2,6 +2,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_RESUME_YAML_PATH?: string;
+  readonly VITE_RESUME_NAME?: string;
+  readonly VITE_RESUME_WEBSITE?: string;
+  readonly VITE_RESUME_LINKEDIN?: string;
+  readonly VITE_RESUME_GITHUB?: string;
 }
 
 interface ImportMeta {
